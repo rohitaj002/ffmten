961456829dbc1b36bceb15d53cdd5560bde90a1f
rohit sah <k97rohit@gmail.com>
2024-01-15 17:40:44 +0530

    modified main ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // TODO: Need to disable in the production mode
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('FFMax10')
+    .setDescription('FFMax10 Api documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('Game')
+    .build();
+
+  /* Add swagger configuration */
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port, () => {
+    logger.log(`Server started at ${port}`);
+  });
 }
 bootstrap();
